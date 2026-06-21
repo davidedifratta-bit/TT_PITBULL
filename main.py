5cb1490f95199a1e4d980418ca2343dc72dfe4a5
davidedifratta-bit <davide.difratta@liceodiazce.edu.it>
2026-06-22 01:46:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,21 +19,7 @@ async def analisi(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data["attesa_match"] = True
 
     await update.message.reply_text(
-        "🏓 Inserisci il match Setka Cup\n\n"
-        "Esempio:\n"
-        "Ivanov vs Petrov"
-    )
-async def setka(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "🏓 SETKA CUP\n\n"
-        "Match demo:\n"
-        "Ivanov vs Petrov\n\n"
-        "Probabilità:\n"
-        "Ivanov 62%\n"
-        "Petrov 38%\n\n"
-        "Fiducia: 7/10\n\n"
-        "Pronostico:\n"
-        "Vittoria Ivanov"
+         "🏓 Inserisci il match TT Cup / Setka Cup"
     )
 
 
@@ -147,7 +133,6 @@ app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("analisi", analisi))
-app.add_handler(CommandHandler("setka", setka))
 app.add_handler(
     MessageHandler(filters.TEXT & ~filters.COMMAND, ricevi_match)
 )
